cd92019f0393c078aad63bc2d3aa72b6739612e8
haranobuhardo <haranobuhardo@gmail.com>
2023-10-25 16:38:59 +0700

    forgot to add AppMenu to app Options

--- main.go
+++ main.go
@@ -33,13 +33,14 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "free-llm-desktop",
-		Width:  1024,
-		Height: 768,
+		Width:  800,
+		Height: 600,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
+		Menu:             AppMenu,
 		Bind: []interface{}{
 			app,
 		},
